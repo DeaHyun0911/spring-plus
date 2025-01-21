26b94ca121341e8c244d6c63b1e5732ed3399446
DeaHyun0911 <eogus0911@gmail.com>
2025-01-21 20:33:48 +0900

    Lv 1-1: Transactional
    - 서비스 로직의 모든 기능에 트랜잭션 ReadOnly 옵션이 적용된 부분 삭제
    - 할일 서비스 중 저장기능에만 트랜잭션이 적용되도록 수정

--- src/main/java/org/example/expert/domain/auth/controller/AuthController.java
+++ src/main/java/org/example/expert/domain/auth/controller/AuthController.java
@@ -7,6 +7,8 @@ import org.example.expert.domain.auth.dto.request.SignupRequest;
 import org.example.expert.domain.auth.dto.response.SigninResponse;
 import org.example.expert.domain.auth.dto.response.SignupResponse;
 import org.example.expert.domain.auth.service.AuthService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +25,9 @@ public class AuthController {
     }
 
     @PostMapping("/auth/signin")
-    public SigninResponse signin(@Valid @RequestBody SigninRequest signinRequest) {
-        return authService.signin(signinRequest);
+    public ResponseEntity<SigninResponse> signin(@Valid @RequestBody SigninRequest signinRequest) {
+        return ResponseEntity.ok()
+            .header(HttpHeaders.AUTHORIZATION, authService.signin(signinRequest).getBearerToken())
+            .body(new SigninResponse(authService.signin(signinRequest).getBearerToken()));
     }
 }
